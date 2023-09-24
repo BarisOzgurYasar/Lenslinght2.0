d39a6c2a0690a820f5dc17c64d2c8be012691295
Barış Özgür Yaşar <yasarbarisozgur@gmail.com>
2023-09-24 19:05:09 +0300

    Lenslight

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -10,5 +10,6 @@ router
   .get(photoController.getAllPhotos);
 
 router.route('/:id').get(photoController.getAPhoto);
+router.route('/:id').delete(photoController.deletePhoto);
 
 export default router;
